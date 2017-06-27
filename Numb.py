4367f9fd284e7222ef6c641573e92863768aff03
Jack Parsons <jack.parsons.uk@gmail.com>
2017-06-27 14:10:21 +0100

    Adding async

--- Numb.py
+++ Numb.py
@@ -1,12 +1,12 @@
 import math
 import random
+from multiprocessing import Pool
 
 lba = int(input("Enter the mean: "))
 po = lambda x: math.e**(-lba) * (lba**x)/math.factorial(x)
 
 
-def cumulative_compare():
-    cumulative = random.random()
+def cumulative_compare(cumulative):
     current = 0
     sum_thing = 0
 
@@ -19,10 +19,11 @@ def cumulative_compare():
 
 
 trials = int(input("How many numbers do you want?: ")) + 1
-po_lice = []
+p = Pool()
+po_lice = p.map(cumulative_compare, (random.random() for _ in range(trials)))
 
-for i in range(trials):
-    po_lice.append(cumulative_compare())
+#for i in range(trials):
+#    po_lice.append(cumulative_compare())
 
 for i in range(max(po_lice) + 1):
     thing = po_lice.count(i)
